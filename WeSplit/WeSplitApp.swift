10c3f3e69deeec63fdbfc18a8237d4eab3ccc4f9
Mike Kostenko <vopros.m@gmail.com>
2023-08-20 21:51:27 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Mike Kostenko on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
