b89ff0784e6c63fbdfb59f3e67a532cc081e00b7
Mike Kostenko <vopros.m@gmail.com>
2024-08-02 13:35:51 +0100

    Refactor TransactionView UI. Add comment functionlality

--- WeSplit/TransactionView.swift
+++ WeSplit/TransactionView.swift
@@ -10,20 +10,60 @@ import SwiftUI
 struct TransactionView: View {
     @State var transaction: HistoricalTransaction
     let currencyID: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currency?.identifier ?? "EUR")
+    @FocusState private var commentIsFocused: Bool
     
     var amount: Double {
         transaction.checkAmount * Double(1 + transaction.tipsPercentage / 100)
     }
     
+    var paid: Double {
+        amount / Double(transaction.people)
+    }
+    
     var body: some View {
         VStack{
-            Text("Total amount: \(transaction.checkAmount, format: currencyID)")
-            Text("Tips: \(transaction.tipsPercentage)%")
-            TextField("Comment", text: $transaction.comment)
-                .padding()
+            HStack{
+                Text("Total amount:")
+                    .foregroundStyle(.secondary)
+                Spacer()
+                Text("\(transaction.checkAmount, format: currencyID)")
+                    .font(.title3.bold())
+            }
+            HStack {
+                Text("Tips:")
+                    .foregroundStyle(.secondary)
+                Spacer()
+                Text("\(transaction.tipsPercentage)%")
+                    .font(.title3).bold()
+            }
+            Spacer()
+                .frame(height: 10)
+            Group{
+                HStack{
+                    Text("Comment:").foregroundStyle(.secondary)
+                    Spacer()
+                }
+                TextField("Type comment", text: $transaction.comment, axis: .vertical)
+                    .padding(4)
+                    .background(RoundedRectangle(cornerRadius: 10).fill(.grayBluewishControl).opacity(0.2))
+                    .focused($commentIsFocused)
+                    .toolbar {
+                        ToolbarItemGroup(placement: .keyboard) {
+                            Spacer()
+                            Button {
+                                commentIsFocused.toggle()
+                            } label: {
+                                Image(systemName: "keyboard.chevron.compact.down")
+                            }
+                        }
+                    }
+            }
+                
             Spacer()
         }
-        .navigationTitle("\(transaction.date.formatted(date: .long, time: .omitted))")
+        .padding()
+        .background(Gradient(colors: [Color(.grayBluewishBg).opacity(0.5), .clear.opacity(0.5)]))
+        .navigationTitle("\(transaction.date.formatted(.dateTime.year(.twoDigits).month(.abbreviated).day(.defaultDigits))): \(paid, format: currencyID)")
     }
 }
 
